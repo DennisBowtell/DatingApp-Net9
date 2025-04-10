28712f0e0ce3f54472d1edf1cb3ca7208429d519
Dennis Bowtell <dennis@baxterit.com.au>
2025-04-10 13:11:17 +1000

    End of Section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,12 +5,17 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { platformBrowser } from '@angular/platform-browser';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     //does not import automatically
-    provideAnimations()
+    provideAnimations(),
+    //npm install ngx-toastr  -  https://npmjs.com/package/ngx-toastr
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
